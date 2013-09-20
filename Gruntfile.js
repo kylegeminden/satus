98add3b6509dbcc601c2b9fcc8717e03230dd2e3
Kyle Geminden <kyle@kylegeminden.com>
2013-09-19 22:44:32 -0600

    Added imagemin.

--- Gruntfile.js
+++ Gruntfile.js
@@ -28,18 +28,7 @@ module.exports = function(grunt) {
     concat: {
       plugins: {
         src: [
-          'js/plugins/transition.js',
-          'js/plugins/alert.js',
-          'js/plugins/button.js',
-          'js/plugins/carousel.js',
-          'js/plugins/collapse.js',
-          // 'js/plugins/dropdown.js',
-          'js/plugins/modal.js',
-          'js/plugins/tooltip.js',
-          'js/plugins/popover.js',
-          'js/plugins/scrollspy.js',
-          'js/plugins/tab.js',
-          'js/plugins/affix.js'
+          'js/plugins/your-plugin.js',
         ],
         dest: 'js/plugins/<%= pkg.name %>.js'
       }
@@ -52,6 +41,17 @@ module.exports = function(grunt) {
       plugins: {
         files: {
           'js/plugins/<%= pkg.name %>.min.js': [ 'js/plugins/<%= pkg.name %>.js' ]
+        }
+      }
+    },
+
+    imagemin: {
+      touchicons: {
+        options: {
+          optimizationLevel: 7
+        },
+        files: {
+          'img/ico/apple-touch-icon-precomposed.png': 'img/ico/apple-touch-icon-precomposed.png'
         }
       }
     },
@@ -81,6 +81,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   //default tasks to run
