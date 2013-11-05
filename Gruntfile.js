96675f297eb391c2430969b2012cdb92e6ad2589
Kyle Geminden <kyle@kylegeminden.com>
2013-11-04 21:59:35 -0700

    Numerous updates...

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,33 +5,19 @@ module.exports = function(grunt) {
         pkg: grunt.file.readJSON('package.json'),
 
         less: {
-            development: {
-                options: {
-                    paths: ['css']
-                },
-                files: {
-                    'css/<%= pkg.name %>.css': [ 'less/satus.less' ]
-                }
-            },
             production: {
                 options: {
                     paths: ['css'],
-                    yuicompress: true,
-                    // report: 'gzip'
+                    // compress: true,
+                    // report: 'gzip',
+                    sourceMap: true,
+                    sourceMapFilename: 'css/<%= pkg.name %>.css.map',
+                    sourceMapRootpath: '../',
                 },
                 files: {
-                    'css/<%= pkg.name %>.min.css': [ 'less/satus.less' ]
-                }
-            }
-        },
-
-        concat: {
-            plugins: {
-                src: [
-                    'js/plugins/your-plugin.js',
-                ],
-                dest: 'js/plugins/<%= pkg.name %>.js'
-            }
+                    'css/<%= pkg.name %>.css': [ 'less/satus.less' ]
+                },
+            },
         },
 
         uglify: {
@@ -40,26 +26,32 @@ module.exports = function(grunt) {
             },
             plugins: {
                 files: {
-                    'js/plugins/<%= pkg.name %>.min.js': [ 'js/plugins/<%= pkg.name %>.js' ]
-                }
-            }
+                    'js/plugins/<%= pkg.name %>.js': [
+                        'js/plugins/yourplugin.js'
+                    ]
+                },
+            },
         },
 
         imagemin: {
             touchicons: {
                 options: {
-                    optimizationLevel: 7
+                    optimizationLevel: 7,
                 },
                 files: {
                     'img/ico/apple-touch-icon-precomposed.png': 'img/ico/apple-touch-icon-precomposed.png'
-                }
-            }
+                },
+            },
         },
 
         watch: {
             less: {
                 files: ['less/**/*.less'],
                 tasks: 'less',
+            },
+            uglify: {
+                files: ['js/plugins/**/*.js'],
+                tasks: 'uglify',
             },
             css: {
                 files: ['css/**/*.css'],
@@ -72,14 +64,19 @@ module.exports = function(grunt) {
                 options: {
                     livereload: true,
                 },
-            }
-        }
+            },
+            js: {
+                files: ['js/**/*.js'],
+                options: {
+                    livereload: true,
+                },
+            },
+        },
 
     });
 
     //load our tasks
     grunt.loadNpmTasks('grunt-contrib-less');
-    grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-imagemin');
     grunt.loadNpmTasks('grunt-contrib-watch');
